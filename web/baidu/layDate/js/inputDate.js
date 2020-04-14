20ec3b2e2e3483e7074f504cb40cd0812d1ae8f4
ly <ly_zszcyx@aliyun.com>
2020-04-14 19:44:16 +0800

    新增layout
    新增日期输入

---
+++ web/baidu/layDate/js/inputDate.js
@@ -0,0 +1,11 @@
+layui.use('laydate', function() {
+    var laydate = layui.laydate;
+
+    //常规用法
+    laydate.render({
+        //elem: document.getElementById("test1")
+        elem: '#test1'
+    });
+})
+
+
